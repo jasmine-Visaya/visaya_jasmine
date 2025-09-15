9e18ca4f935053e9e9fbe1ba3e172f5afa2c1d85
Jasmine <visayajasminegrace2004@gmail.com>
2025-09-15 21:09:08 +0800

    First commit

--- app/views/users/create.php
+++ app/views/users/create.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign Up</title>
-  <link rel="stylesheet" href="<?=base_url();?>public/style.css">
+  <link rel="stylesheet" href="<?=base_url();?>/public/style.css">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
