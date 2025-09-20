28d23b823f9417ab2065ba6afeed4db8e6dbde8a
Jasmine <visayajasminegrace2004@gmail.com>
2025-09-20 21:40:52 +0800

    commit

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -37,7 +37,7 @@ class UsersController extends Controller {
             'page_delimiter' => '&page='
         ]);
         $this->pagination->set_theme('bootstrap'); // or 'tailwind', or 'custom'
-        $this->pagination->initialize($total_rows, $records_per_page, $page, site_url('users').'?q='.urlencode($q));
+        $this->pagination->initialize($total_rows, $records_per_page, $page, site_url('users').'?q='.$q);
         $data['page'] = $this->pagination->paginate();
         $this->call->view('users/UsersData', $data);
     
